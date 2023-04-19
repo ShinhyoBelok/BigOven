651e1d91bbb053d0b3a2f7c56da73953d4605187
ShinhyoBelok <shibo6937@gmail.com>
2023-04-19 11:55:58 -0400

    Update ability file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,9 +5,10 @@ class Ability
     # Define abilities for the user here. For example:
     #
     return unless user.present?
-
-    can %i[read create destroy], Food, user_id: user.id
-    can %i[read create destroy], Recipe, user_id: user.id
+    
+    can :read, :all
+    can %i[create update destroy], Food, user_id: user.id
+    can %i[create update destroy], Recipe, user_id: user.id
     
     return unless user.role == 'admin'
 
