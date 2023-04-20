9ae33c494d80e5cafeef55d6f6b42a50f844fa9b
Glenda Diaz <glendadiazah@outlook.com>
2023-04-20 15:56:22 -0600

    Add factoies for tests

---
+++ spec/factories/user.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+    factory :user do
+      name { Faker::Name }
+      sequence(:email) { |n| "user_#{n}@factory.com" }
+      password {Faker::Alphanumeric}
+    end
+  end
+  
